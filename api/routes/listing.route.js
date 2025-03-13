ea8cd792601a7d5e83bf329f820c2ccf29059bda
SeneshAnujaya <senesh9637@gmail.com>
2025-03-13 21:42:43 +0530

    create search route controller and search functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createListing, deleteListing, getListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, getListings, updateListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
 router.post('/update/:id', verifyUser, updateListing);
-router.get('/get/:id', getListing)
+router.get('/get/:id', getListing);
+router.get("/get", getListings)
 
 export default router;
