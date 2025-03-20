b44c3457f5451980eea48d50bc295a9253b17d37
SeneshAnujaya <senesh9637@gmail.com>
2025-03-20 19:23:14 +0530

    complete home page

--- client/src/components/Listingitem.jsx
+++ client/src/components/Listingitem.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from 'react-icons/md'
 
 const Listingitem = ({listing}) => {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[270px]'>
         <Link to={`/listing/${listing._id}`}>
         <img src={listing.imageUrls[0]} alt='listing cover' className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'/>
      
