52af02c04222fa8e67789a05817d1dfcc33f5990
SeneshAnujaya <senesh9637@gmail.com>
2025-03-20 19:27:14 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad nulla similique repellendus repudiandae impedit, assumenda tempora quibusdam placeat consequuntur dolorum laborum iste veritatis fugiat aliquam doloremque natus pariatur aliquid eos.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, facilis porro quo facere, esse repellat pariatur consectetur inventore accusantium expedita debitis. Quos nostrum exercitationem veritatis ut eius, modi deserunt obcaecati.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad nulla similique repellendus repudiandae impedit, assumenda tempora quibusdam placeat consequuntur dolorum laborum iste veritatis fugiat aliquam doloremque natus pariatur aliquid eos.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, facilis porro quo facere, esse repellat pariatur consectetur inventore accusantium expedita debitis. Quos nostrum exercitationem veritatis ut eius, modi deserunt obcaecati.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad nulla similique repellendus repudiandae impedit, assumenda tempora quibusdam placeat consequuntur dolorum laborum iste veritatis fugiat aliquam doloremque natus pariatur aliquid eos.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, facilis porro quo facere, esse repellat pariatur consectetur inventore accusantium expedita debitis. Quos nostrum exercitationem veritatis ut eius, modi deserunt obcaecati.</p>
+    </div>
   )
 }
 
